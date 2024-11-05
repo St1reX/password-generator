ac65e6fe44524af2055c08880626dd78344d6bd1
Jakub Uryga <urygajakub@gmail.com>
2024-11-05 19:36:55 +0100

    read from file implemented

--- UserKeyWords.py
+++ UserKeyWords.py
@@ -3,22 +3,31 @@ import csv23
 
 
 class UserKeyWords:
+
     def __init__(self):
+        self.keyword_list_directory = pathlib.Path(__file__).parent.absolute() / 'assets'
+        self.keyword_list_file = self.keyword_list_directory / 'keyword_list.csv'
+
         self.keyword_list = []
 
-    @staticmethod
+        self.create_keyword_list()
+
     def create_keyword_list(self):
-        keyword_list_path = pathlib.Path(__file__).parent.absolute() / 'assets'
+        if not self.keyword_list_directory.exists():
+            self.keyword_list_directory.mkdir()
 
-        if not keyword_list_path.exists():
-            keyword_list_path.mkdir()
-
-        keyword_list_file = keyword_list_path / 'keyword_list.csv'
-
-        if not keyword_list_file.exists():
-            with open(keyword_list_file, 'w', newline='') as new_file:
+        if not self.keyword_list_file.exists():
+            with open(self.keyword_list_file, 'w', newline=''):
                 pass
+
+    def fetch_keyword_list_from_file(self):
+        with open(self.keyword_list_file, 'r', newline='') as csv_file:
+            csv_reader = csv23.reader(csv_file)
+
+            for row in csv_reader:
+                self.keyword_list.append(row)
+                print(row)
 
 
 test1 = UserKeyWords()
-test1.create_keyword_list()
+test1.fetch_keyword_list_from_file()
